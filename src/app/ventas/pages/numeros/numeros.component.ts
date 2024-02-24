c2ca41d3abfa154e4e3e400e84ae227137d7b304
yonny <yonnydominguez22@gmail.com>
2024-02-23 19:00:18 -0500

    feat: Pipes porcentajes y decimales

--- src/app/ventas/pages/numeros/numeros.component.ts
+++ src/app/ventas/pages/numeros/numeros.component.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-numeros',
   templateUrl: './numeros.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class NumerosComponent {
-
+  ventasNetas: number = 2452782.3452;
+  porcentajes: number = 0.46;
 }
