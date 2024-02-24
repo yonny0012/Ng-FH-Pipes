6aff12ae6273f647917e1d186159b9685b96a046
yonny <yonnydominguez22@gmail.com>
2024-02-24 09:31:35 -0500

    feat: Pipes basicos terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -13,22 +14,26 @@ import { registerLocaleData } from '@angular/common';
 registerLocaleData(localEs);
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule, AppRouterModule, VentasModule],
-  providers: [
-    {provide: LOCALE_ID, useValue:'es-CU'}
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    AppRouterModule,
+    VentasModule,
   ],
+  providers: [{ provide: LOCALE_ID, useValue: 'es-CU' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
-/** 
- * Para configurar el idioma de la 
+/**
+ * Para configurar el idioma de la
  * aplicacion de forma global se usa:
  *  // para seleccionar idioma
  *  import localEs from '@angular/common/locales/es-CU';
- *  // para regitrar idioma 
+ *  // para regitrar idioma
  * import { registerLocaleData } from '@angular/common';
- * 
+ *
  * luego se ubica en los providers como un objeto con los parametros:
  *  {provide: LOCALE_ID, useValue:'es-CU'}
  * // estos deven ser importados
-*/
+ */
--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -4,11 +4,11 @@ import { CommonModule } from '@angular/common';
 import { MenubarModule } from 'primeng/menubar';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
-
+import { FieldsetModule } from 'primeng/fieldset';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MenubarModule, CardModule, ButtonModule,],
+  exports: [MenubarModule, CardModule, ButtonModule, FieldsetModule],
 })
 export class PrimeNgModule {}
